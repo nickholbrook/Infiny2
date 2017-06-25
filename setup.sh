af3e2c9f19013b3363c59c493a604b92beb1bd34
nickholbrook <nick.holbrook@gmail.com>
2017-06-25 20:40:30 +0100

    Renamed script names

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git clone https://github.com/nickholbrook/Infiny2.git ~/Infiny02
+cd ~/Infiny02/
+clear
+echo "Enter 'new-flask-host.sh' to create a new Flask server instance"
+echo "To delete an existing flask server, type del-flask-host.sh' 
+
