3e6b444b0560680c76cbf1a35dbc631353cc3f48
nickholbrook <nick.holbrook@gmail.com>
2017-06-26 07:39:27 +0100

    Added URL details for Flask Webapp

--- new-flask-host.sh
+++ new-flask-host.sh
@@ -2,7 +2,12 @@
 
 aws cloudformation deploy --template-file createInfinyBlog.json --stack-name Infiny
 sleep 10s
+#aws cloudformation describe-stacks --stack-name Infiny
 ip_addr=$(aws cloudformation describe-stacks --stack-name Infiny | grep OutputValue | awk -F ":" '{print $2}')
 
+ip_message="Input the following address in your browser address bar: http://"
+ip_port=":5000"
+
 clear
-echo $ip_addr
+echo ${ip_message}${ip_addr}${ip_port}
+
